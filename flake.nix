01107d9b593dd220300a6deae80c29a74c2d5d29
guu <n0n3ofyourbus1nes5@outlook.com>
2022-12-20 16:14:22 +0100

    Initial (untested) restructuring, added flakes

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "I'm scared T.T"
+
+  inputs = {
+    nixpkgs.url  = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs-f2k.url = "github:fortuneteller2k/nixpkgs-f2k";
+  };
+
+  outputs = { self, nixpkgs, home-manager, nixpkgs-f2k, ... } @inputs:
+    let
+      lib      = nixpkgs.lib;
+      overlays = with inputs; [
+        (final: _:
+          let inherit (final) system;
+          in (with nixpkgs-f2k.packages.${system}; {
+            awesome = awesome-git;
+            picom   = picom-git;
+          })
+        )
+        nixpkgs-f2k.overlays.default
+      ];
+    in {
+      nixosConfiguration = {
+        fakeflake = import ./hosts/fakeflake {
+          inherit config nixpkgs overlays inputs system home-manager;
+        };
+      };
+    }
+}
