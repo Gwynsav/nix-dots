6d560b51d16c540e229176a072f925064ea6ecce
guu <n0n3ofyourbus1nes5@outlook.com>
2022-12-20 12:50:53 +0100

    Initial Commit (not even tested)

---
+++ configuration.nix
@@ -0,0 +1,72 @@
+# ---------------------------- #
+# -- nix configuration file -- #
+# ---------------------------- #
+
+{ config, pkgs, ... };
+
+{
+  imports = [
+      ./hardware-configuration.nix
+    ];
+
+    # Bootloader
+    # ----------
+    boot.loader.systemd-boot.enable      = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    # Networking
+    # ----------
+    networking.hostname                  = "fakeflake";
+    networking.networkmanager.enable     = true;
+    # networking.proxy.default = "https://user:password@proxy.port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    # networking.firewall.enable           = true;
+    # networking.firewall.allowedTCPPorts  = [ ... ];
+    # networking.firewall.allowedUDPPorts  = [ ... ];
+
+    # Locale
+    # ------
+    time.timeZone      = "Europe/Amsterdam";
+    i18n.defaultLocale = "en_US.UTF-8";
+    console = {
+      font         = "Lat2-Terminus16";
+      keyMap       = "us";
+      useXkbConfig = true;
+    };
+
+    # Users
+    # -----
+    users.users.gw = {
+      isNormalUser = true;
+      extraGroups  = [ "wheel" ];
+      packages     = with pkgs; [ ];
+    };
+
+    # X Server
+    # --------
+    services.xserver = {
+      enable       = true;
+      layout       = "us";
+      xkbOption    = "caps:super";
+      # Touchpad support
+      libinput.enable = true;
+    };
+
+    # Packages
+    # --------
+    environment.systemPackages = with pkgs; [
+      vim
+      git
+      wget
+      firefox
+    ];
+
+    # NixOS Settings
+    # --------------
+    # CUPS support
+    services.printing.enable       = true;
+    # Copy configuration.nix to /run/current-system/
+    system.copySystemConfiguration = true;
+    # Initial OS release.
+    system.stateVersion            = "22.11";
+}
