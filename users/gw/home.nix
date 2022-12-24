04df134b569bec8fd7bf5e9f0f86a4ab513ae155
Gwynsav <n0n3ofyourbus1nes5@outlook.com>
2022-12-24 15:45:43 +0100

    Gonna remove that 0.2%

--- users/gw/home.nix
+++ users/gw/home.nix
@@ -11,10 +11,11 @@ in
 {
   # Home Manager variables
   # ----------------------
-  home.username      = "gw";
-  home.homeDirectory = "/home/gw";
-  # It is not necessary to change this with updates.
-  home.stateVersion            = "22.11";
+  home = {
+    username      = "gw";
+    homeDirectory = "/home/gw";
+    stateVersion  = "22.11";
+  };
   programs.home-manager.enable = true;
 
   # Program Configurations
